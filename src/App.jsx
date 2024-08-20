b53769550c06df9ea60ff9d538a28208feb0cabc
jib2004 <obasaajibola04@gmail.com>
2024-08-20 21:52:25 +0100

    Fixed the hamburger menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
 import Navbar from "./components/Navbar"
 import { useState } from "react"
 import { GiHamburgerMenu } from "react-icons/gi";
+import { useSelector } from "react-redux";
+
 
 
 function App({children}) {
   const [displayMenu,setDisplayMenu] = useState(false)
+  const {theme} = useSelector(state=>state.theme)
 
   const handleMenu = () =>{
    setDisplayMenu(!displayMenu)
   }
   return (
     <>
-    <div className={`py-2 xl:hidden ${displayMenu ? ' hidden': 'block'}`}>
-    <GiHamburgerMenu onClick={handleMenu} className={`size-[24px] xl:hidden ${displayMenu ? ' hidden': 'block'}`} />
+    <div className={`py-2 px-2  xl:hidden ${displayMenu ? ' hidden': 'block'} ${theme && 'bg-[#0D0B1C] text-white'}`}>
+    <GiHamburgerMenu onClick={handleMenu} className={`size-[24px] xl:hidden ${displayMenu ? ' hidden': 'block'} `} />
     </div>
     <div className="flex">
      
